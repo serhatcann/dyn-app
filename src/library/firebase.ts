8a0abb95b58993063f62a0cd1c7b987bdd639f53
Serhat Can <serhatcan1996@gmail.com>
2023-02-17 18:17:18 +0300

    tailwind and react router added, env file bugfix

--- src/library/firebase.ts
+++ src/library/firebase.ts
@@ -4,17 +4,15 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.FIREBASE_PROJECT_ID,
-	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.FIREBASE_APP_ID,
+	apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env
+		.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
 };
+const firebaseApp = initializeApp(firebaseConfig);
 
-initializeApp(firebaseConfig);
-const auth = getAuth();
-const firestore = getFirestore();
-
-export { firestore };
-export { auth };
+export const firestore = getFirestore(firebaseApp);
+export const auth = getAuth(firebaseApp);
