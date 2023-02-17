944fa28fa77323c90f97a1ef0e6cd22de0e12c5a
Serhat Can <serhatcan1996@gmail.com>
2023-02-17 18:28:40 +0300

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 function App() {
-	return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+	return (
+		<div>
+			<h1 className='text-center text-3xl font-bold'>Hello world!</h1>
+			<Routes>
+				<Route path='/' element={<SignIn />} />
+				<Route path='/signup' element={<SignUp />} />
+				<Route path='/home' element={<Home />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
